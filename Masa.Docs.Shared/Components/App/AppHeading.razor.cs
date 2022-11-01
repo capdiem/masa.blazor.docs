f58d0c9b491194b58d21e81dc1fe309af08edf50
capdiem <capdiem@live.com>
2022-11-01 18:02:54 +0800

    🚧 feat: working on navs

---
+++ Masa.Docs.Shared/Components/App/AppHeading.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Masa.Docs.Shared.Components;
+
+public class AppHeading : ComponentBase
+{
+    [Parameter]
+    public int Level { get; set; } = 1;
+
+    [Parameter]
+    public string? Href { get; set; }
+
+    [Parameter, EditorRequired]
+    public string Content { get; set; }
+
+    private static Dictionary<int, string> _map = new()
+    {
+        { 1, "text-h3 text-sm-h3 mb-4" },
+        { 2, "text-h4 text-sm-h4 mb-3" },
+        { 3, "text-h5 mb-2" },
+        { 4, "text-h6 mb-2" },
+        { 5, "text-subtitle-1 font-weight-medium mb-2" },
+    };
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.OpenElement(0, $"h{Level}");
+        builder.AddAttribute(1, "class", $"m-heading {_map[Level]}");
+        builder.AddContent(2, (builder) =>
+        {
+            builder.OpenElement(0, "a");
+            builder.AddAttribute(1, "href", Href);
+            builder.AddAttribute(2, "class", "text-decoration-none text-right text-md-left");
+            // builder.AddAttribute(3, "click", );
+            builder.AddContent(4, "#");
+            builder.CloseElement();
+        });
+        builder.AddContent(3, Content);
+        builder.CloseElement();
+    }
+}
---
+++ Masa.Docs.Shared/Services/AppService.cs
@@ -0,0 +1,131 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Masa.Docs.Shared.Services;
+
+public class AppService
+{
+    private readonly HttpClient _httpClient;
+
+    private readonly Lazy<Task<List<NavItem>>> _navs;
+
+    public AppService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+
+        _navs = new Lazy<Task<List<NavItem>>>(() =>
+        {
+            var navs = httpClient.GetFromJsonAsync<List<NavItem>>("_content/Masa.Docs.Shared/data/nav.json", new JsonSerializerOptions()
+            {
+            });
+
+            return navs;
+        });
+    }
+
+    public async Task<List<NavItem>> GetNavs()
+    {
+        return await _navs.Value;
+    }
+}
+
+public class NavItemJsonConverter : JsonConverter<NavItem>
+{
+    public override NavItem? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.PropertyName)
+        {
+            throw new JsonException();
+        }
+
+        var navItem = new NavItem();
+
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndObject)
+            {
+                return navItem;
+            }
+
+            if (reader.TokenType == JsonTokenType.PropertyName)
+            {
+                var propertyName = reader.GetString();
+                reader.Read();
+                switch (propertyName)
+                {
+                    case "title":
+                        navItem.Title = reader.GetString();
+                        break;
+                    case "icon":
+                        navItem.Icon = reader.GetString();
+                        break;
+                    case "group":
+                        navItem.Group = reader.GetString();
+                        break;
+                    case "items":
+                        navItem.Items = ReadSubItems(ref reader);
+                        break;
+                }
+            }
+        }
+
+        throw new JsonException();
+    }
+
+    public override void Write(Utf8JsonWriter writer, NavItem value, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+
+    private List<NavItem> ReadSubItems(ref Utf8JsonReader reader)
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+        {
+            throw new JsonException();
+        }
+
+        var navItems = new List<NavItem>();
+
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                var title = reader.GetString();
+                navItems.Add(new NavItem(title));
+            }
+            else if (reader.TokenType == JsonTokenType.StartObject)
+            {
+                
+            }
+        }
+
+        return navItems;
+    }
+}
+
+public class NavItem
+{
+    public string Title { get; set; }
+
+    public string Group { get; set; }
+
+    public string Icon { get; set; }
+
+    public List<NavItem> Items { get; set; }
+
+    public NavItem()
+    {
+    }
+
+    public NavItem(string title)
+    {
+        Title = title;
+    }
+}
